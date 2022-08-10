73bdcb96cd3f6242add7d16f8314ce4acd0b20c2
dekatutorial <110667526+dekatutorial@users.noreply.github.com>
2022-08-10 14:34:03 +0700

    Update gotoYT.js

--- gotoYT.js
+++ gotoYT.js
@@ -1,3 +1,3 @@
 function tutor() {
-  setTimeout(function(){window.location.replace("https://youtube.com/dekatutorial")}, 5000);
+  setTimeout(function(){window.location.replace("https://youtube.com/dekatutorial")}, 10000);
 }
