b733cb97b513da10b08d84901433374654049958
dekatutorial <110667526+dekatutorial@users.noreply.github.com>
2022-08-13 09:58:23 +0700

    Create mauungkapin.js

---
+++ mauungkapin.js
@@ -0,0 +1,21 @@
+function link(){
+  var link = document.createElement("div");
+  link.classList.add("link");
+  link.setAttribute("style", "position: fixed; z-index: 9999; bottom: 15px; left: 15px;");
+  var body = document.querySelector("body");
+  body.appendChild(link);
+
+  var divLink = document.querySelector(".link");
+  var aYT = document.createElement("a");
+  aYT.setAttribute("href", "https://www.youtube.com/dekatutorial");
+  aYT.setAttribute("style", "text-decoration: none; color: white; opacity: 0.25; display: block; font-size: 0.9em;");
+  aYT.innerHTML = "Youtube : Deka Tutorial";
+
+  var aTT = document.createElement("a");
+  aTT.setAttribute("href", "https://www.tiktok.com/@deka_tutorial");
+  aTT.setAttribute("style", "text-decoration: none; color: white; opacity: 0.25; display: block; font-size: 0.9em;");
+  aTT.innerHTML = "Tiktok : @deka_tutorial";
+
+  divLink.appendChild(aYT);
+  divLink.appendChild(aTT);
+}
