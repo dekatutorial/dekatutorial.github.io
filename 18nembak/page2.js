c0ac3f27d852f6efafcd5b4d5f72384ef7fff12e
dekatutorial <110667526+dekatutorial@users.noreply.github.com>
2022-10-18 14:53:09 +0700

    Add files via upload

--- 18nembak/page2.js
+++ 18nembak/page2.js
@@ -1,5 +1,5 @@
 var html =
-  '<h2>Pesan Berhasil Terkirim</h2><h3>Anda akan diarahkan ke halaman utama</h3><h4 class="timer">10</h4><p>Dibuat Oleh :</p><a href="" class="yt"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/1280px-YouTube_full-color_icon_%282017%29.svg.png" height="30px" alt="" srcset="" />Deka Tutorial</a><div class="banner"><p>Tutorial Script Bucin</p><a href=""><img src="bannerYT.png" alt="" /></a></div>';
+  '<h2>Pesan Berhasil Terkirim</h2><h3>Anda akan diarahkan ke halaman utama</h3><h4 class="timer">10</h4><p>Dibuat Oleh :</p><a href="https://youtube.com/DekaTutorial" class="yt"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/1280px-YouTube_full-color_icon_%282017%29.svg.png" height="30px" alt="" srcset="" />Deka Tutorial</a><div class="banner"><p>Tutorial Script Bucin</p><a href="https://youtube.com/DekaTutorial"><img src="https://dekatutorial.github.io/18nembak/bannerYT.png" alt="" /></a></div>';
 
 document.querySelector("body").innerHTML += html;
 
